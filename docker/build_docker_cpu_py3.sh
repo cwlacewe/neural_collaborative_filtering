19528f455f4286422adf9de8c7a5213bc98bbf80
Chaunte W Lacewell <chaunte.w.lacewell@intel.com>
2018-12-11 16:58:31 -0800

    Updated docker files

--- docker/build_docker_cpu_py3.sh
+++ docker/build_docker_cpu_py3.sh
@@ -1,3 +1,2 @@
 # Builds docker image
-sudo docker build $DOCKER_PROXY_BUILD_ARGS \
-    -t ncf_keras:cpu_py3 -f Dockerfile_cpu_py3 .
+sudo docker build $DOCKER_PROXY_BUILD_ARGS -t ncf_keras:cpu_py3 -f Dockerfile_cpu_py3 .
